c5175281d4be5fee4496812a5c2da6295d9575f3
Dru Hiyas <czhiyas@gmail.com>
2026-03-20 18:27:51 +0800

    updated theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,21 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mds: {
+          navy: "#284b7b",
+          cyan: "#10bad7",
+          sky: "#6fd6e8",
+          mist: "#e8f4f7",
+          ink: "#17314e",
+          slate: "#5e738c",
+        },
+      },
+      boxShadow: {
+        mds: "0 18px 50px rgba(23, 49, 78, 0.14)",
+      },
+    },
   },
   plugins: [],
 }
-
